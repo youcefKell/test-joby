85f9b6a95d61fc444deb2238590b0a7b756b7afb
youcef kellali <kell9ycf@gmail.com>
2026-06-05 01:43:12 +0100

    Update recruter section

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -29,7 +29,7 @@ const Dashboard = () => {
             {/* Navbar for Recuriter Panel */}
             <div className='shadow py-4'>
                 <div className='px-5 flex justify-between items-center'>
-                    <img onClick={e => navigate('/')} className='max-sm:w-32 cursor-pointer' src={assets.logo} alt="" />
+                    {/* <img onClick={e => navigate('/')} className='max-sm:w-32 cursor-pointer' src={assets.logo} alt="" /> */}
                     {companyData && (
                         <div className='flex items-center gap-3'>
                             <p className='max-sm:hidden'>Welcome, {companyData.name}</p>
